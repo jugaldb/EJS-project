2b585686bdd61fe00d64d9c2a666f99349d6fbf5
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-06-29 12:58:10 +0530

    completed

--- app.js
+++ app.js
@@ -21,5 +21,18 @@ app.get('/contact',function(req,res){
 	res.render('pages/contact')
 })
 
+app.get('/add/',function(req,res){
+	var x = req.query.firstNumber
+	var y = req.query.secondNumber
+	var z= Number(x)+Number(y)
+	res.send('answer:'+z)
+})
+app.get('/calcBMI/',function(req,res){
+	var x = req.query.weight
+	var y = req.query.height
+	var z= (Number(x)/(Number(y)*Number(y))).toFixed(2)
+	res.send('answer:'+z)
+})
+
 app.listen(8080||process.env.PORT);
 console.log('8080 is the magic port');
